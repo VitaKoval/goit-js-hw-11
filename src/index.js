80c309dd0eb7cfbd00fc1412e114ac670e0e9efb
Vita Koval <mayevit@hotmail.com>
2022-09-01 14:01:03 +0300

    1-ysk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import { forFetchPixabay } from './js/forFetchPixabay';
+import {
+  forFetchPixabay,
+  incrementPage,
+  onTotalPages,
+} from './js/forFetchPixabay';
 import { createGalleryCards, gallery } from './js/createGalleryCards';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 
+// слушаем форму поиска картинок
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(evt) {
@@ -16,25 +22,61 @@ function handleSubmit(evt) {
 
   // Значение в инпуте
   const query = evt.target.elements;
-  const searchQuery = query.searchQuery.value;
+  const searchQuery = query.searchQuery.value.trim();
 
   // HTTP-запрос на сервер
-  forFetchPixabay(searchQuery).then(({ data: { hits, totalHits } }) => {
-    if (totalHits === 0) {
-      Notify.info(
-        'Sorry, there are no images matching your search query. Please try again.',
-        {
-          position: 'center-center',
-          width: '700px',
-          fontSize: '20px',
-        }
-      );
-    } else {
-      Notify.success(`Hooray! We found ${totalHits} images.`);
-    }
+  forFetchPixabay(searchQuery)
+    .then(({ data: { hits, totalHits } }) => {
+      if (totalHits === 0) {
+        Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.',
+          {
+            position: 'center-center',
+            width: '700px',
+            fontSize: '20px',
+          }
+        );
+      } else {
+        Notify.success(`Hooray! We found ${totalHits} images.`);
+      }
 
-    gallery.innerHTML = '';
-    gallery.insertAdjacentHTML('beforeend', createGalleryCards(hits));
-    lightbox.refresh();
-  });
+      gallery.innerHTML = '';
+      gallery.insertAdjacentHTML('beforeend', createGalleryCards(hits));
+      lightbox.refresh();
+      loadMoreBtn.classList.remove('is-hidden');
+      incrementPage();
+
+      let calcTotalPages = onTotalPages(hits.length);
+      // console.log('calcTotalPages', calcTotalPages);
+      if (calcTotalPages >= totalHits) {
+        loadMoreBtn.classList.add('is-hidden');
+        Notify.info("We're sorry, but you've reached the end of search results.")
+      }
+    })
+    .catch(error => {
+      gallery.innerHTML = '';
+    });
+
+  // слушаем кнопку load More
+  loadMoreBtn.addEventListener('click', handleMoreClick);
+
+  function handleMoreClick(evt) {
+    forFetchPixabay(searchQuery)
+      .then(({ data: { hits, totalHits } }) => {
+        gallery.insertAdjacentHTML('beforeend', createGalleryCards(hits));
+        lightbox.refresh();
+        incrementPage();
+        let calcTotalPages = onTotalPages(hits.length);
+        // console.log('calcTotalPages', calcTotalPages);
+        
+        if (calcTotalPages >= totalHits) {
+          loadMoreBtn.classList.add('is-hidden');
+          Notify.info("We're sorry, but you've reached the end of search results.")
+        }
+      })
+      .catch(error => {
+        gallery.innerHTML = '';
+        loadMoreBtn.classList.add('is-hidden');
+      });
+  }
 }
--- src/js/forFetchPixabay.js
+++ src/js/forFetchPixabay.js
@@ -1,15 +1,33 @@
 import axios from 'axios';
+
+let page = 1;
+const per_page = 4;
+let totalPages = 0;
+
 axios.defaults.baseURL = 'https://pixabay.com/api';
 axios.defaults.params = {
   key: '29605366-6e90110b2388a1c27ba35efcd',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
+  per_page: per_page,
 };
 
 export function forFetchPixabay(searchQuery) {
   return axios.get('/', {
     params: {
-  q: searchQuery,
+      q: searchQuery,
+      page: page,
 }})
 }
+
+export function incrementPage() {
+  page += 1;
+  console.log('page', page)
+}
+
+export function onTotalPages(newTotal) {
+  return totalPages += newTotal;
+}
+
+
